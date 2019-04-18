cfd7ec1d414acd76cb7c3da18a24fafe2cff3898
feven <feventsegay6@gmail.com>
2019-04-18 16:02:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,7 +30,7 @@ def using_flatten(array)
   array.flatten()
 end
 def using_delete(array,string)
-  array.delete(1)
+  array.delete(1,string)
 end
 def using_delete_at(array,integer)
   array.delete(integer)
